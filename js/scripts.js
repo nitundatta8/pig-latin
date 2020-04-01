bdf6258201a4f72151f704f1fd82bae1b67b61d0
nitunwa <nitun.2@gmail.com>
2020-04-01 15:57:44 -0700

    add functionality to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,80 @@
+function translate(input) {
 
-$(document).ready(function(){
-  $("#calculate").click(function(event){
-     console.log("value");
-     var countBy = $("#countBy").val();
-     var countTo = $("#countTo").val();
-     if(!isNumber(countBy)){alert("Please enter a valid number.")
-      };
-     if(!isNumber(countTo)){alert("Please enter a valid number.")};
-     var countIn = parseInt(countBy);
-     var countOut = parseInt(countTo);
-     if(countIn < countOut){
-      console.log(calculate(countIn,countOut));
-      $(".output").show();
-      $(".output").text(" Result is :" +calculate(countIn,countOut))
-     }
-     else{
-      alert("Count To should be bigger than Count By");
-     }
-    event.preventDefault();
-  });
-});
+    var aplhabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var vowels = ["a", "e", "i", "o", "u"];
+    var consonants = [ "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+    "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
- function isNumber(number){
-   if(number !== '' && !isNaN(number) && number>0 ){
-     return true;
-   }
-   else{return false}
- } 
+    var words = input.split(" ");
+    var output = "";
+    var ending = "";
+    var endingConsonants = "";
+    var vowelFound = false;
+    var piglatinOutput = "";
 
- 
+    for(var index = 0; index < words.length; index++) {
 
-function calculate(countBy,countTo){
-var result = "";
-  for(var i=1;i<= countTo;i++){
-    if((i % countBy) === 0){
-      result += " " + i;
+      var letters = words[index].toLowerCase();
+      ending = "";
+      output = "";
+      endingConsonants = "";
+
+      for(var index2 = 0; index2 < letters.length; index2++) {
+        if(!aplhabet.includes(letters[index2])) {
+          output += letters[index2];
+        }
+        else if(letters.length === 1 && vowels.includes(letters[index2])) {
+          output += letters[index2];
+          ending = "way";
+        }
+        else if(letters.length === 1 && consonants.includes(letters[index2])) {
+          output += letters[index2];
+          ending = "ay";
+        }
+        else if(vowels.includes(letters[0])){
+          output += letters[index2];
+          ending = "way";
+        }
+        else if(consonants.includes(letters[0])){
+          if(!vowelFound) {
+            if(consonants.includes(letters[index2])) {
+              endingConsonants += letters[index2];
+            }
+            else {
+              if(letters[index2] === "u" && letters[index2-1] === "q") {
+                vowelFound = true;
+                endingConsonants += letters[index2];
+              }
+              else {
+                output += letters[index2];
+                vowelFound = true;
+              }
+            }
+          }
+          else {
+            output += letters[index2];
+          }
+          ending = "ay";
+        }
+        else {
+          output += letters[index];
+        }
+      }
+      piglatinOutput += output + ending + " ";
     }
-  }
-  return result;
-}
+    
+    return piglatinOutput;
+};
+
+$(document).ready(function() {
+
+  $("#piglatin").submit(function(event) {
+      event.preventDefault();
+
+      var input = $("#sentence").val();
+      var output = translate(input);
+
+      $("#output").text(output);
+  }); 
+});
