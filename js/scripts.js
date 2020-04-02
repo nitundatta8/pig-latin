339e705aebf2e20c81b7fd44c3a44c123779128f
nitundatta8 <62224052+nitundatta8@users.noreply.github.com>
2020-04-02 13:22:48 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,90 @@
-function translate(input) {
-
-    var aplhabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var vowels = ["a", "e", "i", "o", "u"];
-    var consonants = [ "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
-    "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-
-    var words = input.split(" ");
-    var output = "";
-    var ending = "";
-    var endingConsonants = "";
-    var vowelFound = false;
-    var piglatinOutput = "";
-
-    for(var index = 0; index < words.length; index++) {
-      var letters = words[index].toLowerCase();
-      ending = "";
-      output = "";
-      endingConsonants = "";
-      vowelFound = false;
-
-      for(var index2 = 0; index2 < letters.length; index2++) {
-
-        if(!aplhabet.includes(letters[index2])) {
-          output += letters[index2];
-        }
+var vowels = ["a", "e", "i", "o", "u"];
+    
+function translate(sentence) {
+    var words = sentence.split(" ");
+    var newWords = [];
+    //non-alphabetical characters
+    newWords = words.map(function(word){
         
-        else if(letters.length === 1 && vowels.includes(letters[index2])) {
-          output += letters[index2];
-          ending = "way";
-        }
-        else if(letters.length === 1 && consonants.includes(letters[index2])) {
-          output += letters[index2];
-          ending = "ay";
-        }
-        else if(vowels.includes(letters[0])){
-          output += letters[index2];
-          ending = "way";
-        }
-        else if(consonants.includes(letters[0])){
-          if(!vowelFound) {
-            if(consonants.includes(letters[index2])) {
-              endingConsonants += letters[index2];
-            }
-            else {
-              if(letters[index2] === "u" && letters[index2-1] === "q") {
-                vowelFound = true;
-                endingConsonants += letters[index2];
-              }
-              else {
-                output += letters[index2];
-                vowelFound = true;
-              }
-            }
-          }
-          else {
-            output += letters[index2];
-          }
-          ending = "ay";
-        }
-        else {
-          output += letters[index];
-        }
-      }
-      piglatinOutput += output + endingConsonants + ending + " ";
-    }
+       if(checkAlpaNumeric(word)){
+         console.log("non letter" + word);
+         return word;
+       }
 
-    return piglatinOutput;
-};
+      var letters = word.split("");
+      // add "way" to single-letter vowel  and adds "ay" to single-letter   
+      if(vowels.includes(letters[0])){
+         return word + "way";
+      }else{
+         if(letters.length === 1){
+           return letters + "ay";
+         }
+           //moves "QU" together if included in the first  consonants of the word  
+       
+           var foundQU = findQu(letters);
+           if(foundQU !== -1){
+           var part1 = letters.slice(0,foundQU +1);
+           var part2 = letters.slice(foundQU +1 , letters.length);
+           var lettersWtQU = part2.join("") + part1.join("") + "ay";
+           return lettersWtQU;
+          }
+           // move Y as a consonant if the world begins with it
+    
+           if(letters[0] === 'y'){
+            
+            var part2 = letters.slice(1 , letters.length);
+            var lettersWtQU = part2.join("") + "y" + "ay";
+            return lettersWtQU;
+           }
+           //moves all consonants before vowel to end
+           var consonantWtVowel = searchVowel(letters);
+           if(consonantWtVowel !== -1){
+            var part1 = letters.slice(0,consonantWtVowel);
+            var part2 = letters.slice(consonantWtVowel , letters.length);
+            var lettersWtQU = part2.join("") + part1.join("") + "ay";
+            return lettersWtQU;
+          }else{
+             return  letters.join("") + "ay";
+          }
+      }
+     return  letters.join("");
+
+    });
+    return newWords;
+  }
+    //squeal
+   function findQu(letters){
+     for(var i=0;i<(letters.length-1);i++){
+       if(letters[i] === "q" && letters[i+1] === "u"){
+        return i+1;
+       }
+     }
+     return -1;
+   }
+   //tractor
+   function searchVowel(letters){
+    for(var i=0;i<letters.length;i++){
+      if(vowels.includes(letters[i])){
+        return i;
+      }
+
+    }
+    return -1;
+   }
+
+
+   function checkAlpaNumeric(word){
+    var non_alp=['1','2','3','4','5','6','7','8','9','0','$','%','@','#','!','^','&','*','(',')','-','_','+','=S']
+     var letters = word.split("")
+     for(var i=0;i<letters.length;i++){
+       if( non_alp.includes(letters[i])){
+         return true;
+       }
+     }
+     return false;
+      
+   }
+    
 
 $(document).ready(function() {
 
@@ -75,17 +92,14 @@ $(document).ready(function() {
       event.preventDefault();
 
       var input = $("#sentence").val();
+      if(input === ''){
+        alert("Please enter a word")
+        return;
+    }
       var output = translate(input);
-
-      $("#output").text(output);
+      console.log(output.join(' '));
+      $("#output").text(output.join(' '));
   }); 
 });
 
 
-// for(var i=0;i<sentence.length;i++){
-  // sentence.forEach(function(char){
-  //    if((!vowels.includes(char)) && (!consonants.includes(char))){
-  //      return sentence;
-  //    }
-  // });
-// }
